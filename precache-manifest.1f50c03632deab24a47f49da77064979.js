eba6de4f116f2325dd81497eb41d5b71c02c1f85
Viviana Damiano <62727615+vvndmn@users.noreply.github.com>
2020-08-12 16:48:17 -0700

    Add files via upload

---
+++ precache-manifest.1f50c03632deab24a47f49da77064979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd03c035854e07838d31817070a281db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bf13574350930045f7e",
+    "url": "/static/css/main.19ae4dc2.chunk.css"
+  },
+  {
+    "revision": "b0f597be5bf5280f10d7",
+    "url": "/static/js/2.87f56ebd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.87f56ebd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf13574350930045f7e",
+    "url": "/static/js/main.c119341d.chunk.js"
+  },
+  {
+    "revision": "505e4dc665c09938bf7e",
+    "url": "/static/js/runtime-main.9075e973.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
